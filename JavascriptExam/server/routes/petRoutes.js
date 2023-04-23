7e168d58a761271978e2827924bdadfcfb2ce08a
David Ramirez Vazquez <ramirezdavid826@gmail.com>
2023-04-23 16:45:44 -0700

    commit

--- JavascriptExam/server/routes/petRoutes.js
+++
@@ -1,9 +0,0 @@
-const PetController = require('../controllers/petControllers');
-
-module.exports = app => {
-    app.get('/api/allPets', PetController.findAllPets);
-    app.post('/api/newPet', PetController.createPet);
-    app.get('/api/onePet/:id', PetController.findOnePet);
-    app.put('/api/updatePet/:id', PetController.updatePet);
-    app.delete('/api/deletePet/:id', PetController.deletePet);
-}
